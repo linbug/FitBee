228b2e922ba1e0b780a791229be085e5769495a3
Lin Taylor <taylorxlin@gmail.com>
2015-09-08 16:21:41 +0100

    First commit

---
+++ Fitocracy_scraper.py
@@ -0,0 +1,29 @@
+# 
+
+
+
+from selenium import webdriver
+#driver = webdriver.PhantomJS()
+
+
+###################### Preamble: execute once
+path_to_chromedriver ='/Users/Lin/Downloads/chromedriver'
+
+driver = webdriver.Chrome(executable_path = path_to_chromedriver)
+
+url = 'https://www.fitocracy.com/'
+driver.set_window_size(1024,768) #if you don't do this, the window size is too small to find the elements
+driver.get(url)
+
+login_xpath = '/html/body/div[2]/div/div/div[2]/a'
+driver.find_element_by_xpath(login_xpath).click()
+
+username = driver.find_element_by_xpath('//*[@id="login-modal-form"]/div[2]/div[1]/input')
+
+
+
+username = driver.find_element_by_xpath('//*[@id="login-modal-form"]/div[2]/div[1]/input')
+username.send_keys("xxxxxxxx")
+password = driver.find_element_by_xpath('//*[@id="login-modal-form"]/div[2]/div[2]/input')
+password.send_keys('xxxxxxxx')
+driver.find_element_by_xpath('//*[@id="login-modal-form"]/button').click()
