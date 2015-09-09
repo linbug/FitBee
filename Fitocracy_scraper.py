4397a06fc7afb28e6e60727f010263f2dae77c49
Lin Taylor <taylorxlin@gmail.com>
2015-09-09 15:11:35 +0100

    minor edits

--- Fitocracy_scraper.py
+++ Fitocracy_scraper.py
@@ -1,9 +1,9 @@
 ##################### Fill out your details
-Fitocracy_username 	= 'your_Fitocracy_username'
-Fitocracy_password 	= 'your_Fitocracy_password'
-Beeminder_authtoken = 'your_Beeminder_auth_token' #get your auth token by signing into Beeminder, and then visiting https://www.beeminder.com/api/v1/auth_token.json
-Beeminder_username	= 'your_Beeminder_username'
-Beeminder_password	= 'your_Beeminder_password'
+Fitocracy_username 	= 'Your_Fitocracy_username'
+Fitocracy_password 	= 'Your_Fitocracy_password'
+Beeminder_authtoken = 'Your_Beeminder_Auth_Token' #get your auth token by signing into Beeminder, and then visiting https://www.beeminder.com/api/v1/auth_token.json
+Beeminder_username	= 'Your_Beeminder_Username'
+Beeminder_goalname	= 'Your_goal_name'
 
 ##################### Import dependencies
 
@@ -16,7 +16,7 @@ from beeminderpy import Beeminder
 
 ###################### Open up Fitocracy in your browser
 
-#driver = webdriver.Chrome(executable_path = path_to_chromedriver)
+driver = webdriver.Chrome(executable_path = path_to_chromedriver)
 url = 'https://www.fitocracy.com/'
 driver.set_window_size(1024,768) #if you don't do this, the window size is too small to find the elements
 driver.get(url)
@@ -32,7 +32,7 @@ def login_to_Fitocracy():
 	time.sleep(2)
 	username.send_keys(Fitocracy_username)
 	password = driver.find_element_by_xpath('//*[@id="login-modal-form"]/div[2]/div[2]/input')
-	time.sleep(2)
+	time.sleep(5)
 	password.send_keys(Fitocracy_password)
 	driver.find_element_by_xpath('//*[@id="login-modal-form"]/button').click()
 	print "Logged in"
@@ -42,15 +42,16 @@ def login_to_Fitocracy():
 
 def scrape_todays_points():
 	print "Scraping points"
+	time.sleep(10)
 	todays_points = driver.find_elements_by_xpath("//div/a[contains(text(),'Today')]/preceding-sibling::span")
 	if todays_points == []:
-	print "No points from today"
+		print "No points from today"
 	total = 0
 	for today in todays_points:   
 	    points = today.find_elements_by_class_name("stream_total_points")[0].text
 	    points = int(points[:-4])
-	    print points
 	    total += points
+	driver.close()
 	return total
 
 def send_points_to_Beeminder(total):
@@ -58,10 +59,10 @@ def send_points_to_Beeminder(total):
 	my_beeminder = Beeminder(Beeminder_authtoken)
 	my_beeminder.create_datapoint(
 		username = Beeminder_username,
-		goalname = Beeminder_password,
+		goalname = Beeminder_goalname,
 		timestamp = int(time.time()),
 		value = total, 
-		comment = 'Scraped from Fitocracy on' + time.ctime())
+		comment = 'Scraped from Fitocracy on ' + time.ctime())
 
 ####################### Execute functions
 
