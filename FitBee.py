603c9d199ce087fd4b77f5b9e60e0d35a01af93f
Lin Taylor <taylorxlin@gmail.com>
2015-09-10 09:34:29 +0100

    Added message that tells you how many points you earned

--- FitBee.py
+++ FitBee.py
@@ -1,25 +1,28 @@
 ##################### Fill out your details
-Fitocracy_username 	= 'Your_Fitocracy_username'
-Fitocracy_password 	= 'Your_Fitocracy_password'
-Beeminder_authtoken = 'Your_Beeminder_Auth_Token' #get your auth token by signing into Beeminder, and then visiting https://www.beeminder.com/api/v1/auth_token.json
-Beeminder_username	= 'Your_Beeminder_Username'
-Beeminder_goalname	= 'Your_goal_name'
+Fitocracy_username 			= 'Your_Fitocracy_username'
+Fitocracy_password 			= 'Your_Fitocracy_password'
+Beeminder_Auth_Token		= 'Your_Beeminder_Auth_Token' 
+Beeminder_username			= 'Your_Beeminder_Username'
+Beeminder_goalname			= 'Your_goal_name'
+path_to_chromedriver 		= '/path/to/chromedriver'
 
 ##################### Import dependencies
 
 import time
 from selenium import webdriver
-path_to_chromedriver ='/Users/Lin/Downloads/chromedriver'
 from beeminderpy import Beeminder
-
+from pyvirtualdisplay import Display
 #driver = webdriver.PhantomJS()
 
 ###################### Open up Fitocracy in your browser
 
+#display = Display(visible=0, size=(800, 600))
+#display.start()
 driver = webdriver.Chrome(executable_path = path_to_chromedriver)
 url = 'https://www.fitocracy.com/'
 driver.set_window_size(1024,768) #if you don't do this, the window size is too small to find the elements
 driver.get(url)
+time.sleep(5)
 
 ###################### Define functions
 
@@ -37,21 +40,20 @@ def login_to_Fitocracy():
 	driver.find_element_by_xpath('//*[@id="login-modal-form"]/button').click()
 	print "Logged in"
 
-# time.sleep(30)
-# driver.set_window_size(1024,768)
-
 def scrape_todays_points():
 	print "Scraping points"
 	time.sleep(10)
 	todays_points = driver.find_elements_by_xpath("//div/a[contains(text(),'Today')]/preceding-sibling::span")
-	if todays_points == []:
-		print "No points from today"
 	total = 0
 	for today in todays_points:   
 	    points = today.find_elements_by_class_name("stream_total_points")[0].text
 	    points = int(points[:-4])
 	    total += points
 	driver.close()
+	if todays_points == []:
+		print "No points from today (maybe something went wrong?) :C"
+	else:
+		print "Today you earned " + str(total) + " points!"
 	return total
 
 def send_points_to_Beeminder(total):
@@ -65,9 +67,10 @@ def send_points_to_Beeminder(total):
 		comment = 'Scraped from Fitocracy on ' + time.ctime())
 
 ####################### Execute functions
-
+driver.set_window_size(1024,768)
 login_to_Fitocracy()
+driver.set_window_size(1024,768)
 total_points = scrape_todays_points()
 send_points_to_Beeminder(total_points)
-
+display.stop()
 
